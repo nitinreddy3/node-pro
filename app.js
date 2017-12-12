e81e0d7a47d086628e3a218871393736c0d12141
nitin reddy <reddynitin3@gmail.com>
2017-12-12 18:01:54 +0530

    Cmd arguments added

--- app.js
+++ app.js
@@ -5,3 +5,19 @@ const _ = require('lodash');
 
 const note = require('./notes');
 
+var command = process.argv[2];
+console.log('Command: ', command);
+
+console.log(process.argv)
+
+if(command === 'add') {
+    console.log("Add new note");
+} else if(command === "list") {
+    console.log("Listing notes");
+} else if(command === "read"){
+    console.log("read a note");
+} else if(command === "remove") {
+    console.log("remove an item");
+} else {
+    console.log("Command not recognised");
+}
