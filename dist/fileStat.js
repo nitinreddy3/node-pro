4f849254b3c089b0c4088d9ecaec740ad9889671
nitin reddy <reddynitin3@gmail.com>
2022-06-03 00:33:44 +0530

    Ignore dist folder

--- dist/fileStat.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-const fs = require('fs');
-fs.stat('test.txt', (err, stats) => {
-    if (err) {
-        console.error(err);
-    }
-    // we have access to the file stats in `stats`
-    console.log(stats);
-});
