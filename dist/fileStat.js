785edbfd8771442f823482b74e84f22aef620255
nitin reddy <reddynitin3@gmail.com>
2022-06-03 00:23:47 +0530

    Worked on the file

--- dist/fileStat.js
+++ dist/fileStat.js
@@ -1,8 +1,9 @@
 "use strict";
 const fs = require('fs');
-fs.stat('/Users/nitin/test.txt', (err, stats) => {
+fs.stat('test.txt', (err, stats) => {
     if (err) {
         console.error(err);
     }
     // we have access to the file stats in `stats`
+    console.log(stats);
 });
--- src/fileStat.ts
+++ src/fileStat.ts
@@ -1,8 +1,9 @@
 const fs = require('fs');
 
-fs.stat('/Users/nitin/test.txt', (err: any, stats: any) => {
+fs.stat('test.txt', (err: any, stats: any) => {
   if (err) {
     console.error(err);
   }
   // we have access to the file stats in `stats`
+  console.log(stats);
 });
